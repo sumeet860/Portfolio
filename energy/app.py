7f66bce5796d5d768af7571f9ac0cd5d4f7ecc6f
sumeet860 <41811254+sumeet860@users.noreply.github.com>
2022-09-10 01:06:58 +0530

    portfolio

---
+++ energy/app.py
@@ -0,0 +1,42 @@
+
+from flask import Flask, url_for, redirect, render_template, request
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+
+
+def write_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_csv(data)
+            return redirect('index.html')
+        except:
+            return 'Form not submitted'
+    else:
+        return 'try again'
+
+if __name__ == "__main__":
+    app.run(debug=True)
